8fdbee8d337871d95e440fe703929435c48908f1
Гирсенок Илья <girsenokil@it2g.ru>
2020-08-17 11:17:48 +0400

    init repo

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, "src", "index.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./build"),
+    compress: true,
+    port: 8000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: ["ts-loader"],
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.js$/,
+        loader: "source-map-loader",
+        enforce: "pre",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".ts", ".js"],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+};
